c8af6864a4eab3a802bdd7ccc7ed99be4d21cfb1
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2024-03-07 15:45:46 -0300

    pequeno erro de 'cálculo de caminho'!
    Fazendo o uploading a cada mudança, porque mexer em códigos entre plataformas sempre ferra com o versionamento.

--- repositorio.py
+++ repositorio.py
@@ -28,37 +28,37 @@ Templates:
 # o que será exportado?
 __all__ = ["cria_novo_repositorio_json"]
 
-from gerenciador import carrega
+from gerenciador import (carrega, CORE_PYTHON)
 from pathlib import Path
 import json
 from banco_de_dados import carregaUR
 
 # caminho compatível tanto com Windows como o Linux.
-CAMINHO_JSON_DATA = Path("data/repositorios.json")
+CAMINHO_JSON_DATA = Path(CORE_PYTHON, "data/repositorios.json")
 
 def cria_novo_repositorio_json() -> None:
-	# carrega dicionário com dados a fazer o JSON, também verifica se ele
-	# tem algum dado.
-	dicionario_dados = carrega()
-	assert len (dicionario_dados) > 0
-	# adiciona último vez que foi feita uma atualização.
-	dicionario_dados["última-atualização"] = carregaUR().timestamp()
+   # carrega dicionário com dados a fazer o JSON, também verifica se ele
+   # tem algum dado.
+   dicionario_dados = carrega()
+   assert len (dicionario_dados) > 0
 
-	with open (CAMINHO_JSON_DATA, "wt", encoding="utf-8") as arquivo_json:
-		json.dump (
-			dicionario_dados, arquivo_json,
-			indent='\t', sort_keys=True,
-			ensure_ascii = False
-		)
-		assert (CAMINHO_JSON_DATA.exists())
-	...
+   # adiciona último vez que foi feita uma atualização.
+   dicionario_dados["última-atualização"] = carregaUR().timestamp()
+   arquivo_json = open (CAMINHO_JSON_DATA, "wt", encoding="utf-8")
+   json.dump (
+      dicionario_dados, arquivo_json,
+      indent='\t', sort_keys=True,
+      ensure_ascii = False
+   )
+   arquivo_json.close()
+   assert (CAMINHO_JSON_DATA.exists())
 
-	if __debug__:
-		with open (CAMINHO_JSON_DATA, "rt", encoding="utf8") as arquivo:
-			obj_json = json.load (arquivo)
-			print (obj_json)
-		...
-	...
+   if __debug__:
+      with open (CAMINHO_JSON_DATA, "rt", encoding="utf8") as arquivo:
+         obj_json = json.load (arquivo)
+         print (obj_json)
+      ...
+   ...
 ...
 
 import unittest
